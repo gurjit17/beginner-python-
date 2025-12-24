2a91ebc97f1cab5ffc10ecdcfd1790b9fdfa1435
pixelperfect <Gurjitramgarhia07@gmail.com>
2025-12-24 16:58:51 +0530

    Add files via upload

---
+++ practice1.py .py
@@ -0,0 +1,46 @@
+#variable types 
+a = 12  
+b = 13 
+print(a+b)
+
+#simple hello world program
+print('hello world')
+
+#check the data type 
+my_name = 'gurjit'
+print(type(my_name))
+value = 1012 
+print(type(value))
+
+#type casting simple program
+str_num = '26'
+int_num =int(str_num)
+print(int_num)
+print(type(int_num))
+
+float = 121.5 
+int_sum = int(float)
+print(int_sum)
+print(type(int_sum))
+
+#explicit type casting 
+#converting string to integer 
+str_num = '26' 
+int_num = int(str_num)
+print(int_sum)
+print(type(int_sum))
+
+#converting a value to boolean 
+bool_1 = (0)
+bool_2 = (1)
+print(bool_1)
+print(bool_2)
+
+#implicit type casting 
+num_int = 19
+num_float = 121.2 
+result = num_int + num_float #integer is automatically 
+print(result)
+print(type(result))
+bool(0)
+bool(1)
