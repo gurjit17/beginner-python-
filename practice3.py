c3f27ba77f69c9c06e90097521eaeb9e39ac3bda
pixelperfect <Gurjitramgarhia07@gmail.com>
2025-12-24 17:42:57 +0530

    Add files via upload

---
+++ practice3.py
@@ -0,0 +1,34 @@
+# In this code we are covered the all operators in python 
+
+#arithmetic operators 
+a = 6
+b = 3 
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+#comparison operators 
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+#assignment oparators
+a = 5
+a = a + 4 
+a = a - 3 
+a = a * 3
+a = a / 3 
+a = a % 3 
+a = a // 3 
+a = a ** 3
+print(a)
+
+x = a>2 and a <5 
+print(x)
